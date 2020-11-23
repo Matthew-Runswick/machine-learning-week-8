041e529dee9add7e9d0075109a606e3350a90d51
Matthew <matthew.runswick@gmail.com>
2020-11-23 20:25:25 +0000

    have part B working, am about to update main function for part C

--- assingment_8.py
+++ assingment_8.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 #Part (i)
 #Part A
@@ -72,65 +73,113 @@ import sys
 num_classes = 10
 input_shape = (32, 32, 3)
 
-# the data, split between train and test sets
-(x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-n=5000
-x_train = x_train[1:n]; y_train=y_train[1:n]
-#x_test=x_test[1:500]; y_test=y_test[1:500]
+def prep_data(number_of_datapoints):
+    # the data, split between train and test sets
+    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+    n=number_of_datapoints
+    x_train = x_train[1:n]; y_train=y_train[1:n]
+    #x_test=x_test[1:500]; y_test=y_test[1:500]
 
-# Scale images to the [0, 1] range
-x_train = x_train.astype("float32") / 255
-x_test = x_test.astype("float32") / 255
-print("orig x_train shape:", x_train.shape)
+    # Scale images to the [0, 1] range
+    x_train = x_train.astype("float32") / 255
+    x_test = x_test.astype("float32") / 255
+    print("orig x_train shape:", x_train.shape)
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
+    # convert class vectors to binary class matrices
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    return x_train, y_train, x_test, y_test
 
-use_saved_model = False
-if use_saved_model:
-	model = keras.models.load_model("cifar.model")
-else:
-	model = keras.Sequential()
-	model.add(Conv2D(16, (3,3), padding='same', input_shape=x_train.shape[1:],activation='relu'))
-	model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
-	model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
-	model.add(Conv2D(32, (3,3), strides=(2,2), padding='same', activation='relu'))
-	model.add(Dropout(0.5))
-	model.add(Flatten())
-	model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(0.0001)))
-	model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
-	model.summary()
+def conv_model_training(x_train, y_train, x_test, y_test, title, penalty, architecture_type):
+    start = time.time()
+    use_saved_model = False
+    if use_saved_model:
+        model = keras.models.load_model("cifar.model")
+    else:
+        # if(architecture_type == "stride"):
+        model = keras.Sequential()
+        model.add(Conv2D(16, (3,3), padding='same', input_shape=x_train.shape[1:],activation='relu'))
+        model.add(Conv2D(16, (3,3), strides=(2,2), padding='same', activation='relu'))
+        model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+        model.add(Conv2D(32, (3,3), strides=(2,2), padding='same', activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Flatten())
+        model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(penalty)))
+        model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+        model.summary()
 
-	batch_size = 128
-	epochs = 20
-	history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
-	model.save("cifar.model")
-	plt.subplot(211)
-	plt.plot(history.history['accuracy'])
-	plt.plot(history.history['val_accuracy'])
-	plt.title('model accuracy')
-	plt.ylabel('accuracy')
-	plt.xlabel('epoch')
-	plt.legend(['train', 'val'], loc='upper left')
-	plt.subplot(212)
-	plt.plot(history.history['loss'])
-	plt.plot(history.history['val_loss'])
-	plt.title('model loss')
-	plt.ylabel('loss'); plt.xlabel('epoch')
-	plt.legend(['train', 'val'], loc='upper left')
-	plt.show()
+        # else:
+        #     model = keras.Sequential()
+        #     model.add(Conv2D(16, (3,3), padding='same', input_shape=x_train.shape[1:],activation='relu'))
+        #     model.add(MaxPooling2D(pool_size=(2, 2)))
+        #     model.add(Conv2D(32, (3,3), padding='same', activation='relu'))
+        #     model.add(MaxPooling2D(pool_size=(2, 2)))
+        #     model.add(Dropout(0.5))
+        #     model.add(Flatten())
+        #     model.add(Dense(num_classes, activation='softmax',kernel_regularizer=regularizers.l1(penalty)))
+        #     model.compile(loss="categorical_crossentropy", optimizer='adam', metrics=["accuracy"])
+        #     model.summary()
 
-preds = model.predict(x_train)
-y_pred = np.argmax(preds, axis=1)
-y_train1 = np.argmax(y_train, axis=1)
-print(classification_report(y_train1, y_pred))
-print(confusion_matrix(y_train1,y_pred))
+        batch_size = 128
+        epochs = 20
+        history = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+        model.save("cifar.model")
+        plt.figure()
+        plt.plot(history.history['accuracy'])
+        plt.plot(history.history['val_accuracy'])
+        plt.title('{} - model accuracy'.format(title))
+        plt.ylabel('accuracy')
+        plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.figure()
+        plt.plot(history.history['loss'])
+        plt.plot(history.history['val_loss'])
+        plt.title('{} - model loss'.format(title))
+        plt.ylabel('loss'); plt.xlabel('epoch')
+        plt.legend(['train', 'val'], loc='upper left')
+        plt.show()
 
-preds = model.predict(x_test)
-y_pred = np.argmax(preds, axis=1)
-y_test1 = np.argmax(y_test, axis=1)
-print(classification_report(y_test1, y_pred))
-print(confusion_matrix(y_test1,y_pred))
+    preds = model.predict(x_train)
+    y_pred = np.argmax(preds, axis=1)
+    y_train1 = np.argmax(y_train, axis=1)
+    print(classification_report(y_train1, y_pred))
+    print(confusion_matrix(y_train1,y_pred))
 
-#Part B (i) - baseline model
+    preds = model.predict(x_test)
+    y_pred = np.argmax(preds, axis=1)
+    y_test1 = np.argmax(y_test, axis=1)
+    print(classification_report(y_test1, y_pred))
+    print(confusion_matrix(y_test1,y_pred))
+
+    end = time.time()
+    print("!!!!!!!!!!!! time to complete training", round(end - start), "s  !!!!!!!!!!!!!!!!!!!!!!!!!")
+
+# x_train, y_train, x_test, y_test = prep_data(5000)
+# conv_model_training(x_train, y_train, x_test, y_test, "5K", 0.0001, "stride")
+
+#Part B (iii)
+# x_train, y_train, x_test, y_test = prep_data(10000)
+# conv_model_training(x_train, y_train, x_test, y_test, "10K", 0.0001, "stride")
+
+# x_train, y_train, x_test, y_test = prep_data(20000)
+# conv_model_training(x_train, y_train, x_test, y_test, "20K", 0.0001, "stride")
+
+# x_train, y_train, x_test, y_test = prep_data(40000)
+# conv_model_training(x_train, y_train, x_test, y_test, "40K", 0.0001, "stride")
+
+# part B (iv)
+x_train, y_train, x_test, y_test = prep_data(5000)
+
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=0", 0, "stride")
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=0.00001 ", 0.00001, "stride")
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=0.0001", 0.0001, "stride")
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=0.001", 0.001, "stride")
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=0.01 ", 0.01, "stride")
+# conv_model_training(x_train, y_train, x_test, y_test, "5K L=1 ", 1, "stride")
+
+#Part C (i)
+
+
+
+
+#dont forget to uncomment all code before submitting
